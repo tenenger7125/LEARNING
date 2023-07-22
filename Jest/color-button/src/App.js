418f8226881502f78b4c02c7dee62295a4f21014
LEE DONG GYU <tenenger7125@gmail.com>
2023-07-22 23:11:56 +0900

    feat: add disable checkbox test code

--- Jest/color-button/src/App.js
+++ Jest/color-button/src/App.js
@@ -3,14 +3,22 @@ import "./App.css";
 
 function App() {
   const [backgroundColor, setBackgroundColor] = useState("red");
+  const [disabled, setDisabled] = useState(false);
 
   const handleClick = () => setBackgroundColor(backgroundColor === "red" ? "blue" : "red");
+  const handleChange = (e) => setDisabled(e.target.checked);
 
   return (
     <div>
-      <button style={{ backgroundColor, color: "white" }} onClick={handleClick}>
+      <button
+        style={{ backgroundColor: disabled ? "gray" : backgroundColor, color: "white" }}
+        onClick={handleClick}
+        disabled={disabled}
+      >
         Change to {backgroundColor === "red" ? "blue" : "red"}
       </button>
+      <input type="checkbox" id="disable-button-checkbox" checked={disabled} onChange={handleChange} />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
--- Jest/color-button/src/App.test.js
+++ Jest/color-button/src/App.test.js
@@ -22,3 +22,52 @@ test("button has correct initial color, text and update when clicked", () => {
   // expect the button text to be 'Change to red'
   expect($button).toHaveTextContent("Change to red");
 });
+
+test("initial conditions", () => {
+  render(<App />);
+
+  // check that the button starts out enabled
+  const $button = screen.getByRole("button", { name: "Change to blue" });
+  expect($button).toBeEnabled();
+
+  // check that the checkbox start out unchecked
+  const $checkbox = screen.getByRole("checkbox");
+  expect($checkbox).not.toBeChecked();
+});
+
+test("체크박스가 체크되어있지 않으면 버튼이 활성화, 체크박스가 체크되어 있으면 버튼이 비활성화", () => {
+  render(<App />);
+
+  const $button = screen.getByRole("button", { name: "Change to blue" });
+  const $checkbox = screen.getByRole("checkbox", { name: "Disable button" });
+
+  expect($checkbox).not.toBeChecked();
+  expect($button).toBeEnabled();
+
+  fireEvent.click($checkbox);
+  expect($checkbox).toBeChecked();
+  expect($button).toBeDisabled();
+});
+
+test("초기상태에서 체크박스가 비활성화이면 빨간색이고, 버튼을 클릭하면 색깔이 파란색으로 변하고 체크박스를 활성화하면 색상은 회색으로 변한다. 그리고 체크박스를 비활성화하면 버튼 색깔은 파란색으로 변한다.", () => {
+  render(<App />);
+
+  const $button = screen.getByRole("button", { name: "Change to blue" });
+  const $checkbox = screen.getByRole("checkbox", { name: "Disable button" });
+
+  expect($checkbox).not.toBeChecked();
+  expect($button).toHaveStyle({ backgroundColor: "red" });
+
+  fireEvent.click($button);
+  expect($button).toHaveStyle({ backgroundColor: "blue" });
+
+  fireEvent.click($checkbox);
+  expect($button).toHaveStyle({ backgroundColor: "gray" });
+  expect($button).toBeDisabled();
+  expect($checkbox).toBeChecked();
+
+  fireEvent.click($checkbox);
+  expect($button).toHaveStyle({ backgroundColor: "blue" });
+  expect($button).toBeEnabled();
+  expect($checkbox).not.toBeChecked();
+});
